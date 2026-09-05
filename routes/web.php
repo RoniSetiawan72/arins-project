e4659b140806b5c58b38bf90ca71d0d78579dd36
Roni Setiawan <ronisetiawannnn1099@gmail.com>
2026-09-06 00:53:55 +0700

    fix: redirect to login on logout and root url

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,14 @@ use App\Http\Controllers\Admin\TenantController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Tenant\TenantDashboardController;
 use App\Models\User;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    if (auth()->check()) {
+        return redirect()->route('dashboard');
+    }
+
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
